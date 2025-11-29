153ba9ff26a546da45e89ddc2895579e7c127ec8
tamakimorito <moritotamaki@gmail.com>
2025-11-29 19:53:22 +0900

    Limit tailwind content scan

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./**/*.{ts,tsx}"],
+  content: [
+    "./index.html",
+    "./**/*.{ts,tsx,html}",
+    "!./node_modules/**/*",
+  ],
   theme: { extend: {} },
   plugins: [],
 };
